7cf26e14338ea7bb34677d2f3feeeea5fe385c6f
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-11 16:14:06 -0700

    removed package

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-
+const moment = require("moment");
 // If you can see 'Hello, World!' in the console, then you can remove this and replace it your code
 console.log('Hello, World!');
 // YOUR JS CODE GOES HERE
+$(document).ready(function() {
+    var today = moment().subtract(10, 'days').calendar();
+
+    $('#currentDay').text(today);
+})
