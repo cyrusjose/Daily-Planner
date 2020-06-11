c77cdedb279e57c3de2340315daf5c1ac452cbf6
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-11 16:23:07 -0700

    created date for the planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-const moment = require("moment");
 // If you can see 'Hello, World!' in the console, then you can remove this and replace it your code
 console.log('Hello, World!');
 // YOUR JS CODE GOES HERE
 $(document).ready(function() {
+    // Create the date for the planner
     var today = moment().subtract(10, 'days').calendar();
-
     $('#currentDay').text(today);
-})
+});
