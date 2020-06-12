8e0cf368ebc451f485bd05f58cc565f6a9031844
Cyrus Jose <cyrus.jose@outlook.com>
2020-06-12 15:55:41 -0700

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,15 @@ console.log('Hello, World!');
 // YOUR JS CODE GOES HERE
 $(document).ready(function() {
     // Create the date for the planner
+
+    const $date = $('.date');
+    const $inputsForm = $('inputs')
+
+    // Display current day on page (##/##/####) format
     var today = moment().subtract(10, 'days').calendar();
     $('#currentDay').text(today);
     //Create the hours
-    console.log(moment().format('h'))
+    // console.log(moment().format('h'))
     var hours = [
         "9AM",
         "10AM",
@@ -24,12 +29,37 @@ $(document).ready(function() {
     // will be used for comparison
     var currentHour = moment().format('h');
     for (var i = 0; i < hours.length; i++) {
-        var timeBlock = $('<div class = "hour row time-block">');
-        var descrptionArea = $('<textArea>');
-        timeBlock.text(hours[i]);
 
-        $('.container').append(timeBlock)
-        $('.container').append(descrptionArea);
+        let $groupDiv = $('<div>');
+        let $hourDiv = $('<div>');
+        let $inputDiv = $('<input>');
+        let $buttonDiv = $('<div>');
+        let $saveButton = $('<button>');
+        let $saveIcon = $('<i>');
+
+
+        $groupDiv.addclass('group mb-3');
+        $inputDiv.addclass('form-control').attr('type', 'text');
+        $hourDiv.addclass('group-append group-text').text(hours[i]);
+        $buttonDiv.addclass('group-append');
+        $saveButton.addclass('btn btn-primary saveButton');
+        $saveIcon.addclass('material-icons').text('save');
+
+        $saveButton.append($saveIcon);
+        $buttonDiv.append($saveButton);
+        $groupDiv.append($hourDiv, $inputDiv, $buttonDiv);
+
+        $inputsForm.append($groupDiv);
+
+
+        
+        // var timeBlock = $('<div class = "hour row time-block">');
+        // var descrptionArea = $('<textArea>');
+        // timeBlock.text(hours[i]);
+        // timeBlock.attr('data-name', hours[i]);
+
+        // $('.container').append(timeBlock)
+        // $('data-name').append(descrptionArea);
 
     }
 });
